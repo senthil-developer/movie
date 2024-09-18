8dd18884ce5b8ff82833cabc1ad4570dfe0c8bf5
senthil <senthildeveloper4@gmail.com>
2024-09-18 18:40:13 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-6">
       <Banner />
-      <Tab path="movie/popular" type="movie" title="Popular Movie" />
-      <Tab path="tv/popular" type="series" title="Popular Series" />
-      <Tab path="person/popular" type="person" title="Popular Person" />
+      <Tab path="trending/movie" type="movie" title="Trending Movie" />
+      <Tab path="trending/tv" type="series" title="Trending Series" />
+      <Tab path="trending/person" type="person" title="Trending Person" />
     </div>
   );
 }
