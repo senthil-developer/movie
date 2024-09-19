80fd52ffa65624aca006c9f3ea7886a66b11ecd6
senthil <senthildeveloper4@gmail.com>
2024-09-19 14:16:05 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -97,7 +97,7 @@ export const Navbar = ({
 const navLinks = [
   { name: "Home", path: "/", icon: <GoHome size={25} />, current_icon: <FaHome size={25} /> },
   { name: "Movie", path: "/movie", icon: <BiMoviePlay size={25} />, current_icon: <BiSolidMoviePlay size={25} /> },
-  { name: "Series", path: "/series", icon: <PiTelevisionSimple size={25} />, current_icon: <PiTelevisionSimpleFull size={25} /> },
+  { name: "Series", path: "/series", icon: <PiTelevisionSimple size={25} />, current_icon: <PiTelevisionSimpleFill size={25} /> },
   { name: "Person", path: "/person", icon: <IoPersonOutline size={25} />, current_icon: <IoPersonSharp size={25} /> },
   { name: "Favorite", path: "/favorite", icon: <TiStarOutline size={25} />, current_icon: <TiStarFullOutline size={25} /> }
 ];
